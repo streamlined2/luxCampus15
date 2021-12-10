4a9ce2ea9f36a0b4ac4a5a512c2f6a6fd35995bd
Serhii Pylypenko <serhii.pylypenko.0@gmail.com>
2021-12-11 00:00:11 +0200

    Ninth draft
    1. added test for ModifyProductServlet class.

--- luxCampus15/src/main/java/org/training/campus/onlineshop/controller/ModifyProductServlet.java
+++ luxCampus15/src/main/java/org/training/campus/onlineshop/controller/ModifyProductServlet.java
@@ -10,11 +10,13 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "ModifyProductServlet", description = "modifies product", loadOnStartup = 1, urlPatterns = "/products/edit")
 public class ModifyProductServlet extends AbstractServlet {
 
+	protected static final String REDIRECTION_RESOURCE = "/WEB-INF/pages/new-edit-product.jsp";
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.getSession().setAttribute(CREATE_PRODUCT_ATTRIBUTE, Boolean.FALSE);
 		req.getSession().setAttribute(PRODUCT_ATTRIBUTE, getProductFromList(req));
-		getServletContext().getRequestDispatcher("/WEB-INF/pages/new-edit-product.jsp").forward(req, resp);
+		getServletContext().getRequestDispatcher(REDIRECTION_RESOURCE).forward(req, resp);
 	}
 
 }
--- luxCampus15/src/test/java/org/training/campus/onlineshop/controller/ModifyProductServletTest.java
+++ luxCampus15/src/test/java/org/training/campus/onlineshop/controller/ModifyProductServletTest.java
@@ -1,15 +1,15 @@
 package org.training.campus.onlineshop.controller;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
@@ -30,7 +30,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.training.campus.onlineshop.entity.Product;
 
 @ExtendWith(MockitoExtension.class)
-class CreateProductServletTest {
+class ModifyProductServletTest {
 
 	@Mock
 	private HttpServletRequest req;
@@ -45,7 +45,7 @@ class CreateProductServletTest {
 	@Mock
 	private RequestDispatcher requestDispatcher;
 	@Mock(answer = Answers.CALLS_REAL_METHODS)
-	private CreateProductServlet servlet;
+	private ModifyProductServlet servlet;
 	@Captor
 	private ArgumentCaptor<Product> productCaptor;
 
@@ -61,16 +61,20 @@ class CreateProductServletTest {
 	@Test
 	void doGetSetsUpCorrectAttributesAndForwards() {
 		try {
+			final long id = 10L;
+			final var name = "Most beautiful picture ever";
+			final var price = BigDecimal.valueOf(1_000_000.00D);
+			final var creationDate = LocalDate.now();
+			final Product sample = Product.builder().id(id).name(name).price(price).creationDate(creationDate).build();
+			when(req.getParameter(AbstractServlet.ITEM_PARAMETER)).thenReturn("0");
+			when(session.getAttribute(AbstractServlet.PRODUCTS_ATTRIBUTE)).thenReturn(List.of(sample));
+
 			servlet.doGet(req, resp);
 
-			verify(session).setAttribute(AbstractServlet.CREATE_PRODUCT_ATTRIBUTE, Boolean.TRUE);
-
+			verify(session).setAttribute(AbstractServlet.CREATE_PRODUCT_ATTRIBUTE, Boolean.FALSE);
 			verify(session).setAttribute(eq(AbstractServlet.PRODUCT_ATTRIBUTE), productCaptor.capture());
 			assertNotNull(productCaptor.getValue());
-			assertEquals(productCaptor.getValue().getId(), 0L);
-			assertSame(productCaptor.getValue().getName(), null);
-			assertSame(productCaptor.getValue().getPrice(), null);
-			assertSame(productCaptor.getValue().getCreationDate(), null);
+			assertSame(productCaptor.getValue(), sample);
 
 			verify(context).getRequestDispatcher(CreateProductServlet.REDIRECTION_RESOURCE);
 			verify(requestDispatcher).forward(req, resp);
